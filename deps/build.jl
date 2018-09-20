cbf51bdf5176276f0a37e84794400fea865d398e
marius <mmillea@ucdavis.edu>
2018-09-21 00:58:29 +0200

    remove dependency on Make

--- deps/build.jl
+++ deps/build.jl
@@ -7,7 +7,7 @@ librecfast = library_dependency("librecfast")
 provides(SimpleBuild,
     (@build_steps begin
         ChangeDirectory(joinpath(BinDeps.depsdir(librecfast),"src","recfast"))
-        `make`
+        `gfortran -ffixed-line-length-none -fPIC -shared -g -O0 recfast.for -o ../../usr/lib/librecfast.so`
     end), librecfast, os = :Unix)
     
 @BinDeps.install Dict([(:librecfast, :librecfast)])
