b560b7eae3b70e5e21abdac7524e6e392a9b89af
marius <mariusmillea@gmail.com>
2020-12-13 19:28:50 -0800

    more cleanup

--- src/Cosmology.jl
+++ src/Cosmology.jl
@@ -2,22 +2,22 @@ module Cosmology
 
 import Roots
 using Base: @kwdef
+using ComponentArrays
 using DelimitedFiles
 using Dierckx
+using DiffEqBase
 using Libdl
+using MacroTools
 using Parameters
 using QuadGK
 using Setfield
-using DiffEqBase
 using Sundials
-using MacroTools
-using ComponentArrays
 using ThreadTools
 
-export new_params, Params, add_derived!,
+export new_params, Params,
     ργ, ρν, ρc, ρb, ρx_over_ωx,
     Hubble, θmc, θs, D_prop, DA, rs, rd, theta2hubble, zstar_HS, zstar,
-    τ, τd, zdrag, rdrag, @self,
+    τ, τd, zdrag, rdrag,
     rs_vis, rd_vis, 
     ⅆrs_ⅆz, ⅆrd²_ⅆz, ⅆτ_ⅆz,
     solve_boltz, 
